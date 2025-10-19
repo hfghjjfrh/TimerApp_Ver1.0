633968b8252ef38a4746c53db93684cd8b6f8a79
hfghjjfrh <hfghjjfrh@gmail.com>
2025-10-19 19:17:56 +0900

    Add files via upload

---
+++ Timer App1/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿
+namespace Timer_App1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Button hour1;
+            this.Start = new System.Windows.Forms.Button();
+            this.display = new System.Windows.Forms.Label();
+            this.min1 = new System.Windows.Forms.Button();
+            this.sec1 = new System.Windows.Forms.Button();
+            this.reset = new System.Windows.Forms.Button();
+            this.sec10 = new System.Windows.Forms.Button();
+            this.min30 = new System.Windows.Forms.Button();
+            this.min10 = new System.Windows.Forms.Button();
+            this.sec30 = new System.Windows.Forms.Button();
+            this.Title = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Stop = new System.Windows.Forms.Button();
+            this.status = new System.Windows.Forms.Label();
+            this.status2 = new System.Windows.Forms.ProgressBar();
+            this.timeup = new System.Windows.Forms.Button();
+            hour1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // hour1
+            // 
+            hour1.AutoEllipsis = true;
+            hour1.BackColor = System.Drawing.Color.DarkOliveGreen;
+            hour1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            hour1.Location = new System.Drawing.Point(495, 313);
+            hour1.Name = "hour1";
+            hour1.Size = new System.Drawing.Size(151, 52);
+            hour1.TabIndex = 13;
+            hour1.Tag = "";
+            hour1.Text = "1Hour";
+            hour1.UseVisualStyleBackColor = false;
+            hour1.Visible = false;
+            hour1.Click += new System.EventHandler(this.Button_SMH1);
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.SystemColors.GrayText;
+            this.Start.Font = new System.Drawing.Font("MS UI Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Start.Location = new System.Drawing.Point(15, 507);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(304, 128);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // display
+            // 
+            this.display.AutoSize = true;
+            this.display.Font = new System.Drawing.Font("MS UI Gothic", 88F);
+            this.display.Location = new System.Drawing.Point(95, 143);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(524, 118);
+            this.display.TabIndex = 2;
+            this.display.Text = "XX:XX:XX";
+            this.display.Click += new System.EventHandler(this.display_Click);
+            // 
+            // min1
+            // 
+            this.min1.BackColor = System.Drawing.Color.Yellow;
+            this.min1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.min1.Location = new System.Drawing.Point(12, 284);
+            this.min1.Name = "min1";
+            this.min1.Size = new System.Drawing.Size(150, 52);
+            this.min1.TabIndex = 4;
+            this.min1.Tag = "";
+            this.min1.Text = "1Minute";
+            this.min1.UseVisualStyleBackColor = false;
+            this.min1.Click += new System.EventHandler(this.Button_SMH1);
+            // 
+            // sec1
+            // 
+            this.sec1.BackColor = System.Drawing.Color.Crimson;
+            this.sec1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.sec1.Location = new System.Drawing.Point(181, 284);
+            this.sec1.Name = "sec1";
+            this.sec1.Size = new System.Drawing.Size(151, 52);
+            this.sec1.TabIndex = 5;
+            this.sec1.Tag = "";
+            this.sec1.Text = "1Second";
+            this.sec1.UseVisualStyleBackColor = false;
+            this.sec1.Click += new System.EventHandler(this.Button_SMH1);
+            // 
+            // reset
+            // 
+            this.reset.BackColor = System.Drawing.Color.Black;
+            this.reset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.reset.Font = new System.Drawing.Font("MS UI Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.reset.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.reset.Location = new System.Drawing.Point(338, 371);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(304, 117);
+            this.reset.TabIndex = 6;
+            this.reset.Text = "Reset";
+            this.reset.UseVisualStyleBackColor = false;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // sec10
+            // 
+            this.sec10.BackColor = System.Drawing.Color.Crimson;
+            this.sec10.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.sec10.Location = new System.Drawing.Point(181, 359);
+            this.sec10.Name = "sec10";
+            this.sec10.Size = new System.Drawing.Size(151, 52);
+            this.sec10.TabIndex = 7;
+            this.sec10.Text = "10Seconds";
+            this.sec10.UseVisualStyleBackColor = false;
+            this.sec10.Click += new System.EventHandler(this.Button_SMH10);
+            // 
+            // min30
+            // 
+            this.min30.BackColor = System.Drawing.Color.Yellow;
+            this.min30.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.min30.Location = new System.Drawing.Point(11, 437);
+            this.min30.Name = "min30";
+            this.min30.Size = new System.Drawing.Size(151, 52);
+            this.min30.TabIndex = 10;
+            this.min30.Text = "30Minutes";
+            this.min30.UseVisualStyleBackColor = false;
+            this.min30.Click += new System.EventHandler(this.Button_SMH30);
+            // 
+            // min10
+            // 
+            this.min10.BackColor = System.Drawing.Color.Yellow;
+            this.min10.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.min10.Location = new System.Drawing.Point(11, 359);
+            this.min10.Name = "min10";
+            this.min10.Size = new System.Drawing.Size(151, 52);
+            this.min10.TabIndex = 9;
+            this.min10.Text = "10Minutes";
+            this.min10.UseVisualStyleBackColor = false;
+            this.min10.Click += new System.EventHandler(this.Button_SMH10);
+            // 
+            // sec30
+            // 
+            this.sec30.BackColor = System.Drawing.Color.Crimson;
+            this.sec30.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.sec30.Location = new System.Drawing.Point(181, 437);
+            this.sec30.Name = "sec30";
+            this.sec30.Size = new System.Drawing.Size(151, 52);
+            this.sec30.TabIndex = 14;
+            this.sec30.Text = "30Seconds";
+            this.sec30.UseVisualStyleBackColor = false;
+            this.sec30.Click += new System.EventHandler(this.Button_SMH30);
+            // 
+            // Title
+            // 
+            this.Title.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("HG正楷書体-PRO", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Title.Location = new System.Drawing.Point(62, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(539, 97);
+            this.Title.TabIndex = 15;
+            this.Title.Text = "Timer App";
+            this.Title.Click += new System.EventHandler(this.Title_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Stop
+            // 
+            this.Stop.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Stop.Font = new System.Drawing.Font("MS UI Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Stop.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Stop.Location = new System.Drawing.Point(338, 507);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(304, 128);
+            this.Stop.TabIndex = 18;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = false;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // status
+            // 
+            this.status.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.status.AutoSize = true;
+            this.status.Font = new System.Drawing.Font("游明朝", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.status.Location = new System.Drawing.Point(338, 261);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(219, 83);
+            this.status.TabIndex = 19;
+            this.status.Text = "NULL";
+            this.status.Click += new System.EventHandler(this.status_Click);
+            // 
+            // status2
+            // 
+            this.status2.Location = new System.Drawing.Point(341, 342);
+            this.status2.Name = "status2";
+            this.status2.Size = new System.Drawing.Size(305, 23);
+            this.status2.TabIndex = 20;
+            this.status2.Click += new System.EventHandler(this.status2_Click);
+            // 
+            // timeup
+            // 
+            this.timeup.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.timeup.CausesValidation = false;
+            this.timeup.Font = new System.Drawing.Font("MS UI Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.timeup.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.timeup.Location = new System.Drawing.Point(-9, -6);
+            this.timeup.Name = "timeup";
+            this.timeup.Size = new System.Drawing.Size(685, 681);
+            this.timeup.TabIndex = 21;
+            this.timeup.Text = "タイムアップ";
+            this.timeup.UseVisualStyleBackColor = false;
+            this.timeup.Click += new System.EventHandler(this.timeup_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 663);
+            this.Controls.Add(this.timeup);
+            this.Controls.Add(this.status2);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.sec30);
+            this.Controls.Add(hour1);
+            this.Controls.Add(this.min30);
+            this.Controls.Add(this.min10);
+            this.Controls.Add(this.sec10);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.sec1);
+            this.Controls.Add(this.min1);
+            this.Controls.Add(this.display);
+            this.Controls.Add(this.Start);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(674, 702);
+            this.MinimumSize = new System.Drawing.Size(674, 702);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Tag = "Button_MSH1";
+            this.Text = "TimerApp Ver1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Label display;
+        private System.Windows.Forms.Button min1;
+        private System.Windows.Forms.Button sec1;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.Button sec10;
+        private System.Windows.Forms.Button min30;
+        private System.Windows.Forms.Button min10;
+        private System.Windows.Forms.Button sec30;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.ProgressBar status2;
+        private System.Windows.Forms.Button timeup;
+    }
+}
+
---
+++ Timer App1/Form1.cs
@@ -0,0 +1,344 @@
+﻿/*
+アプリ名： タイマーVer.1
+　作成者： 外田良平
+作成日時： 2024/8/4(Sun)
+作成言語： C#コンソールアプリケーション
+備　　考： 特にありません
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer_App1
+{
+    public partial class Form1 : Form
+    {
+        int h;
+        int m;
+        int s;
+        int time;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;
+            Init();
+            status.Text = ("Standby");
+            Start.Enabled = false;
+            Stop.Enabled = false;
+            timeup.Visible = false;
+
+        }
+
+        public void Init()
+        {
+            h = 0;
+            m = 0;
+            s = 0;
+            time = 0;
+            WriteTimeText();
+           
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public void WriteTimeText()
+        {
+            display.Text = String.Format("{0:00}:{1:00}:{2:00}",h, m, s);//00:00:00
+        }
+        private void Form1_Load(object sender, EventArgs e) {} //枠の近くの設定をします
+        private void Title_Click(object sender, EventArgs e) {}
+        private void display_Click(object sender, EventArgs e) {} //初期値  XX:XX:XXとしてください
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+            time = time - 1000;
+            m = time / 60000;
+            h = time / (360 * 100*100);
+            s = (time % 60000) / 1000;
+            
+
+
+            WriteTimeText();
+            if (time == 0)
+            {
+              
+                timer1.Stop();
+              
+                status.Text = ("Time Up");
+                status2.Minimum = 0;
+                status2.Maximum = 0;
+                status2.Value = 0;
+
+                try
+                {
+                    SoundPlayer simpleSound = new SoundPlayer(@"..\..\sound\Alarm.wav");
+                    simpleSound.Play();
+                }
+                catch (System.IO.FileNotFoundException)
+                {
+                    MessageBox.Show("サウンドファイルが見つかりません。\nファイルが指定の場所にあるか確認してください。\n", "エラー");
+                }
+                
+                timeup.Visible = true;
+                status2.Visible = false;
+
+                Init();
+                status.Text = ("Standby");
+                min1.Enabled = true;
+                min10.Enabled = true;
+                min30.Enabled = true;
+                sec1.Enabled = true;
+                sec10.Enabled = true;
+                sec30.Enabled = true;
+                Start.Enabled = false;
+                Stop.Enabled = false;
+             
+
+                return;
+                
+            }
+
+        }
+        private void reset_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SoundPlayer simpleSound = new SoundPlayer(@"..\..\sound\chimes.wav");
+                simpleSound.Play();
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                MessageBox.Show("サウンドファイルが見つかりません。\nファイルが指定の場所にあるか確認してください。\n", "エラー");
+            }
+            timer1.Stop();
+            Start.Enabled = false;
+            h = 0;
+            m = 0;
+            s = 0;
+            time = 0;
+            display.Text = String.Format("00:00:00");
+            status.Text = ("Reset");
+            Start.Enabled = false;
+            status2.Minimum = 0;
+            status2.Maximum = 0;
+            status2.Value = 0;
+            min1.Enabled = true;
+            min10.Enabled = true;
+            min30.Enabled = true;
+            sec1.Enabled = true;
+            sec10.Enabled = true;
+            sec30.Enabled = true;
+            Start.Enabled = false;
+            Stop.Enabled = false;
+        }
+
+        
+        
+        private void Start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SoundPlayer simpleSound = new SoundPlayer(@"..\..\sound\notify.wav");
+                simpleSound.Play();
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                MessageBox.Show("サウンドファイルが見つかりません。\nファイルが指定の場所にあるか確認してください。\n", "エラー");
+            }
+            
+            time = (h * 60 * 60 + m * 60 + s) * 1000;　//{(時×60)+(分×60)+ 秒}*1000＝全体の秒　（例）1:30 → 90と計算します
+            timer1.Start();
+            status.Text = ("Start");//　　　　↑ミリ秒を秒に直すためにつけます
+            status2.Style = ProgressBarStyle.Marquee;
+            status2.Maximum = 10;
+            status2.MarqueeAnimationSpeed = 30;
+            min1.Enabled = false;
+            min10.Enabled = false;
+            min30.Enabled = false;
+            sec1.Enabled = false;
+            sec10.Enabled = false;
+            sec30.Enabled = false;
+            Start.Enabled = false;
+            Stop.Enabled = true;
+        }
+
+        private void Button_SMH1(object sender, EventArgs e)
+        {
+            try
+            {
+                SoundPlayer simpleSound = new SoundPlayer(@"..\..\sound\chimes.wav");
+                simpleSound.Play();
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                MessageBox.Show("サウンドファイルが見つかりません。\nファイルが指定の場所にあるか確認してください。\n", "エラー");
+            }
+            
+            Start.Enabled = true;
+            Button btn = (Button)sender;
+
+            if (btn == min1)
+            {
+                m++;
+                if (m >= 60)
+                {
+                    m = 0;
+                }
+
+            }
+            else if (btn == sec1)
+            {
+                s++;
+                if (s >= 60)
+                {
+                    s = 0;
+                }
+            }
+
+            /*
+            else if (btn == hour1)
+            {
+                h++;
+                if (h >= 2)
+                {
+                    h = 0;
+                }
+            }
+            */
+
+            WriteTimeText();
+
+            
+        } //1ずつ増える
+
+        private void Button_SMH10(object sender, EventArgs e)
+        {
+            try
+            {
+                SoundPlayer simpleSound = new SoundPlayer(@"..\..\sound\chimes.wav");
+                simpleSound.Play();
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                MessageBox.Show("サウンドファイルが見つかりません。\nファイルが指定の場所にあるか確認してください。\n", "エラー");
+            }
+            Start.Enabled = true;
+            Button btn = (Button)sender;
+            if (btn == min10)
+            {
+                m = m + 10;
+                if (m >= 60)
+                {
+                    m = 0;
+                }
+
+            }
+            else if (btn == sec10)
+            {
+                s = s + 10;
+                if (s >= 60)
+                {
+                    s = 0;
+                }
+            }
+
+            WriteTimeText();
+        }//10ずつ増える
+
+        private void Button_SMH30(object sender, EventArgs e)
+        {
+            try
+            {
+                SoundPlayer simpleSound = new SoundPlayer(@"..\..\sound\chimes.wav");
+                simpleSound.Play();
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                MessageBox.Show("サウンドファイルが見つかりません。\nファイルが指定の場所にあるか確認してください。\n", "エラー");
+            }
+            Start.Enabled = true;
+            Button btn = (Button)sender;
+            if (btn == min30)
+            {
+                m = m + 30;
+                if (m >= 60)
+                {
+                    m = 0;
+                }
+
+            }
+            else if (btn == sec30)
+            {
+                s = s + 30;
+                if (s >= 60)
+                {
+                    s = 0;
+                }
+            }
+        
+
+            WriteTimeText();
+        }//30ずつ増える
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SoundPlayer simpleSound = new SoundPlayer(@"..\..\sound\chimes.wav");
+                simpleSound.Play();
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                MessageBox.Show("サウンドファイルが見つかりません。\nファイルが指定の場所にあるか確認してください。\n", "エラー");
+            }
+
+            timer1.Stop();
+            WriteTimeText();
+            status.Text = ("Stop");
+            status2.Style = ProgressBarStyle.Marquee;
+            status2.MarqueeAnimationSpeed = 0;
+            
+            min1.Enabled = true;
+            min10.Enabled = true;
+            min30.Enabled = true;
+            sec1.Enabled = true;
+            sec10.Enabled = true;
+            sec30.Enabled = true;
+            Start.Enabled = true;
+            Stop.Enabled = false;
+        }
+
+        private void status_Click(object sender, EventArgs e){}
+
+        private void status2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timeup_Click(object sender, EventArgs e)
+        {
+            try {
+                SoundPlayer simpleSound = new SoundPlayer();
+                simpleSound.Stop();
+                SoundPlayer simpleSound2 = new SoundPlayer(@"..\..\sound\chimes.wav");
+                simpleSound2.Play();
+            } catch (System.IO.FileNotFoundException) {
+                MessageBox.Show("サウンドファイルが見つかりません。\nファイルが指定の場所にあるか確認してください。\n", "エラー");
+            }
+            timeup.Visible = false;
+            status2.Visible = true;
+            
+           
+        }
+    }
+}
